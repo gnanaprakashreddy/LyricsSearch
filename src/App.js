294857d87e6c21826590113d35619018eb2dd224
gnanaprakashreddy <42853286+gnanaprakashreddy@users.noreply.github.com>
2020-12-03 12:38:52 +0530

    Final Commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Navbar from './components/layout/Navbar'
 import Index from './components/layout/Index'
+import Lyrics from './components/tracks/Lyrics'
 import {Provider} from './context'
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import './App.css';
@@ -14,7 +15,8 @@ class App extends Component {
           <Navbar/>
           <div className="container">
             <Switch>
-              <Route exact path='/' component={Index} />  
+              <Route exact path='/' component={Index} />
+              <Route exact path='/lyrics/track/:id' component={Lyrics} />
             </Switch>
           </div>
         </React.Fragment>
